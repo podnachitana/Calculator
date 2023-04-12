a3e8fe475369d24e56a605992c51e921ad5049ab
podnachitana <t.zudina1@gmail.com>
2023-04-12 17:56:35 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Tatiana Zudina on 10.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+//		let gradientLayer = CAGradientLayer()
+//		gradientLayer.frame = view.bounds
+//		gradientLayer.colors = [UIColor.systemGray.cgColor, UIColor.systemPink.cgColor, UIColor.black.cgColor]
+//		view.layer.addSublayer(gradientLayer)
+	}
+
+
+}
+
